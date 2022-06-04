a3b6a2a11228af3cfd5c8b188f94a896c317b971
Dmitrii Ageev <>
2022-06-04 08:51:26 -0400

    Convert CRL files from DER to PEM format

--- dmitrii-ageev.openssl-gost/templates/usr/local/bin/update-local-ca-certificates.sh
+++ dmitrii-ageev.openssl-gost/templates/usr/local/bin/update-local-ca-certificates.sh
@@ -1,13 +1,22 @@
 #!/bin/bash
 
-CA_DIR={{ openssl__local_certs_dir }}
+CA_DIR="/usr/local/share/ca-certificates"
+mkdir -p $CA_DIR
 cd ${CA_DIR}-ru
 ls * | while read file; do
-    hashname="$(openssl x509 -hash -in "${CA_DIR}-ru/$file" -inform DER -noout 2>/dev/null).0"
+    hashname="$(openssl x509 -hash -in "${CA_DIR}-ru/$file" -inform DER -noout 2>/dev/null)" || \
+    hashname="$(openssl crl -hash -in "${CA_DIR}-ru/$file" -inform DER -noout 2>/dev/null)"
     if [ $? -ne 0 ]; then
-      cp -f "${CA_DIR}-ru/$file"  "$CA_DIR/$file"
+      cp -f "${CA_DIR}-ru/$file"  "${CA_DIR}/"
     else
-      openssl x509 -in "${CA_DIR}-ru/$file" -inform DER -outform PEM -out "$CA_DIR/$hashname" &>/dev/null
+      rm -f "${CA_DIR}/${hashname}*" &>/dev/null
+      openssl x509 -in "${CA_DIR}-ru/$file" -inform DER -outform PEM -out "${CA_DIR}/${hashname}" &>/dev/null && \
+	      mv "${CA_DIR}/$hashname" "${CA_DIR}/${hashname}.crt" && \
+	      ln -sf "${CA_DIR}/${hashname}.crt" "${CA_DIR}/${hashname}.0"
+
+      openssl crl -in "${CA_DIR}-ru/$file" -inform DER -outform PEM -out "${CA_DIR}/${hashname}" &>/dev/null && \
+	      mv "${CA_DIR}/$hashname" "${CA_DIR}/${hashname}.crl" && \
+	      ln -sf "${CA_DIR}/${hashname}.crl" "${CA_DIR}/${hashname}.r0"
     fi
 done
-/usr/sbin/update-ca-certificates &>/dev/null
+/usr/sbin/update-ca-certificates --fresh &>/dev/null
